1e049455af9914d812e3695ea7bd701064b5f27d
paulrajarshi0066 <141010252+paulrajarshi0066@users.noreply.github.com>
2025-07-24 11:20:43 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,37 @@
+print("Helo Pythong in PyCharm")
+print(20+50)
+print('20+50')
+print("20+50")
+print(10>5)
+a=3
+b=7
+print('a value',a,'b value',b)
+print(id(a),id(b))
+c="hello"
+#print(c+a)
+string1='Hello'
+print('He' in string1)
+l=[10,20,35]
+print(10 in l)
+print(10 not in l)
+
+
+x=10
+y=10
+print(x is y , x==y)
+print(x is not y , x!=y)
+
+l=[1,'ab',"aabbb"]
+t=(10,'10',5)
+dist={
+    'a1':'aaa',
+    'b1':'bbbb'
+}
+print(l,type(l),t,type(t),dist,type(dist))
+l[1]=10
+print(l)
+ss={a,b,c,'a','a'}
+print(ss,type(ss))
+
+xx=input("enter value xx")
+print(xx)
